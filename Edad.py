6039c7f05ddf0a15b59f231d96e9c4d0b3c37529
William Murcia <wifernando.2603.murcia@gmail.com>
2026-02-24 11:08:19 -0500

    Add files via upload

---
+++ Edad.py
@@ -0,0 +1,14 @@
+x = int(input("Ingrese su edad"))
+
+print("Su edad es", x )
+
+if x<=10:
+    print("Ud Es un niño")
+elif 10 < x < 18:
+    print("Ud es menor de edad")
+elif 18 <= x < 70:
+    print("Ud es mayor de edad")
+else:
+    print("Ud esta cerca de morir")
+    
+    
